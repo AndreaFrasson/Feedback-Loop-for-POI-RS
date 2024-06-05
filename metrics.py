ffdb6f38b91e64c919ee0979ec9cb52f7c8ac15e
AndreaFrasson <frass99and@gmail.com>
2024-06-05 17:06:25 +0200

    rog metric

--- metrics.py
+++ metrics.py
@@ -3,6 +3,7 @@ import numpy as np
 from scipy import stats
 from skmob.measures.individual import radius_of_gyration
 import skmob
+import utils
 
 
 def _uncorrelated_entropy_individual(interactions, normalize=True):
@@ -34,13 +35,58 @@ def _uncorrelated_entropy_individual(interactions, normalize=True):
     return entropy
 
 
-def compute_uncorrelated_entropy(interactions, group_attr, value_attr):
+
+def uncorrelated_entropy(interactions, group_attr, value_attr):
     df = interactions.groupby(group_attr).apply(lambda x: _uncorrelated_entropy_individual(x[value_attr]))
     return pd.DataFrame(df).reset_index().rename(columns={0: 'entropy'})
 
 
+def topk_history(dataframe, k = 10):
+        d_index = []
+
+        topk_items = dataframe['venue_id'].value_counts().index[:k]
+
+        for idx, row in dataframe.iterrows():
+            if row['venue_id'] not in topk_items:
+                d_index.append(idx)
+
+        return d_index
+
+
+
+
 def compute_rog(interactions, k = None):
-    locations_df = pd.read_csv('data/foursquare_complete.csv', sep = ',')
-    interactions_df = pd.DataFrame(interactions.numpy())
+
+    pois_df = pd.read_csv('foursquare/foursquare.item')
+    pois_df.columns = ['venue_id', 'category', 'lat', 'lon']
+
+    iid_field = interactions.iid_field
+    uid_field = interactions.uid_field
+
+    users = utils._from_ids_to_int(interactions.id2token(interactions.uid_field, interactions.inter_feat[uid_field]))
+    items = utils._from_ids_to_int(interactions.id2token(interactions.iid_field, interactions.inter_feat[iid_field]))
+
+    interactions = pd.DataFrame({'uid':users, 'venue_id':items})
+
+    # get the trajectory dataframe
+    tdf = skmob.TrajDataFrame(interactions.merge(pois_df[['venue_id', 'lat', 'lon']], how = 'left'), latitude='lat', longitude='lon', user_id='uid')
+
     if k is None: # compute the global radius of gyrations
+        rog = radius_of_gyration(tdf, False)
+        return rog
+    
+    if k == 0:
+        print('k can\'t be 0')
         return
+    
+    # keep only the k most visited locations, for each user
+
+    # find the indexes to drop
+    drop_list = tdf.groupby('uid').apply(topk_history, k = k)
+    # drop the 'rarest' locations
+    tdf = tdf.drop([item for row in drop_list for item in row])
+
+    #compute the ROG
+    rog_k = radius_of_gyration(tdf, False)
+
+    return rog_k
