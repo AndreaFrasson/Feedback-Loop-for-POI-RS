97d44782fc97f6883e25faac47fb472afabc96e7
Andrea Frasson <a.frasson@studenti.unipi.it>
2024-07-08 10:05:45 +0200

    experiment

--- run_NFM.py
+++ run_NFM.py
@@ -4,7 +4,7 @@ import sys
 from Feedback_Loop import FeedBack_Loop
 
 # SETTINGS CONTEXT BASED
-MODEL = 'FNN'
+MODEL = 'NFM'
 DATA_PATH = os.getcwd() 
 TOP_K = 10
 DATASET = 'foursquare'
@@ -59,7 +59,7 @@ if __name__ == '__main__':
 
 
     fl = FeedBack_Loop(config_dict, m)
-    fl.loop(MaxIt, 'r', True, 'FNN.hyper')
+    fl.loop(MaxIt, 'r', True, 'NFM.hyper')
 
     # save output
     with open('output/'+fl.config['model']+'_'+str(m)+'-'+str(MaxIt)+'.txt','w') as data:  
