7fec36fe73e6aaeed8e41360817bf5e0d9d46020
AndreaFrasson <frass99and@gmail.com>
2024-07-18 16:47:02 +0200

    fix

--- Feedback_Loop.py
+++ Feedback_Loop.py
@@ -172,6 +172,8 @@ class FeedBack_Loop():
 
         if rows_not_active is not None:
             active_users = users - set(rows_not_active)
+        else:
+            active_users = None
 
         not_active_users = np.random.choice(list(active_users), int(len(active_users) * k))
         not_active_rows = not_active_users - 1
