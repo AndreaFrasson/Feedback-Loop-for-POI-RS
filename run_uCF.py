2f35fb987265620b88b71181ff8bc02044056420
AndreaFrasson <frass99and@gmail.com>
2024-07-25 09:14:07 +0200

    user CF

---
+++ run_uCF.py
@@ -0,0 +1,53 @@
+from preprocess import preprocess
+import os
+import sys
+from Feedback_Loop import FeedBack_Loop
+from ind_Pop import ind_Pop
+import json
+
+# SETTINGS
+MODEL = 'ind_Pop'
+DATA_PATH = os.getcwd() 
+TOP_K = 10
+DATASET = 'foursquare'
+EPOCHS = 20
+DEVICE_ID = '0'
+
+# Default parameters
+
+
+
+if __name__ == '__main__':
+    # total arguments
+    n = len(sys.argv)
+    if n < 5:
+        len_step = 5
+        epochs = 20
+    else:
+        len_step = int(sys.argv[1])
+        epochs = int(sys.argv[2])
+        not_rec = sys.argv[3]
+        k = float(sys.argv[4])
+
+    # make the atomic files form the data
+    seed = 1234 # to get always the same users in train/test
+    preprocess(seed)
+
+
+    config_dict = {
+            'model': MODEL,
+            'data_path': DATA_PATH,
+            'top_k': TOP_K,
+            'dataset': DATASET,
+            'epochs': EPOCHS,
+            'use_gpu': len(DEVICE_ID) > 0,
+            'device_id': DEVICE_ID,
+        }
+
+
+    fl = FeedBack_Loop(config_dict, not_rec)
+    fl.loop(epochs, len_step, k = k, user_frac=0, tuning=False)
+
+        # save output
+    with open('output/ind_Pop_'+not_rec+'_'+str(len_step)+'-'+str(epochs)+'_'+str(k)+'.txt','w') as data:  
+      json.dump(fl.metrics, data)
