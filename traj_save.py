74c514e3b6b388699de649335f89b6c37be129d8
Andrea Frasson <a.frasson@studenti.unipi.it>
2024-09-16 19:38:05 +0200

    exp

--- traj_save.py
+++ traj_save.py
@@ -10,7 +10,7 @@ from recbole.quick_start.quick_start import get_model, get_trainer
 
 
 # SETTINGS GENERAL RECOMMENDER
-MODEL = 'MulitVAE'
+MODEL = 'iCF'
 DATA_PATH = os.getcwd() 
 TOP_K = 10
 DATASET = 'foursquare'
@@ -90,6 +90,7 @@ if __name__ == '__main__':
 
 
     for j in range(5):
+        print(j)
 
         for i in range(len_step):
             #recommender choices
